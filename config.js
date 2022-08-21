bce07fa9589f2a1f226ab38ab78b3a0b01f31cd8
linhphuog <111694222+linhphuog@users.noreply.github.com>
2022-08-21 21:30:00 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Linh Phương';
+const giftUrl = 'https://www.facebook.com/pham.lin.96155';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Chúc Mừng Sinh Nhật';
+const contentCard = 'Để tớ nói với bạn điều này nhé: bạn là đứa “điên” nhất trong hội bạn của tớ, ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'image.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
